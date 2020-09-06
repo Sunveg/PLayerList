8b9b1d57e3e0694267c05c5c00221c7d986147d7
Sunveg <sunvegnalwar7@gmail.com>
2020-09-07 00:04:28 +0530

    Updates

---
+++ precache-manifest.6c521f35d7f8d2c69dbfb25dbf5e73fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6543f1f1b894e9fe6953b57b4cea3246",
+    "url": "/PlayerList/index.html"
+  },
+  {
+    "revision": "da9df25a566acbf0f1f2",
+    "url": "/PlayerList/static/css/main.c8453595.chunk.css"
+  },
+  {
+    "revision": "3548082f319e9e724a74",
+    "url": "/PlayerList/static/js/2.c6374fe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PlayerList/static/js/2.c6374fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9df25a566acbf0f1f2",
+    "url": "/PlayerList/static/js/main.6199fe1e.chunk.js"
+  },
+  {
+    "revision": "a31a117258f5d86ffd74",
+    "url": "/PlayerList/static/js/runtime-main.465ec57d.js"
+  }
+]);
